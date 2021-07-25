51a1c492a994a474f8159b948cd3c47bd6296c8d
Erdem <erdemtas.dev@gmail.com>
2021-07-25 13:34:15 +0300

    Function name change, correct input.

--- main.swift
+++ main.swift
@@ -7,19 +7,18 @@
 
 import Foundation
 
-func isPow(num : Int) {
-   //log2() helps us to check if we can write the number in log2 format
+func isPowOfTwo(num : Int) {
+   //log2() prebuilt func. helps us to check if we can write the number in log2 format
     if log2(Double(num)).truncatingRemainder(dividingBy: 1.0)  == 0.0 {
         print(true)
     } else {
         print(false)
     }
     
-    
 }
 
 
 
-print(log2(Double(4)).truncatingRemainder(dividingBy: 1.0))
+print(isPowOfTwo(num: 9))
 
 
